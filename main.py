18d9545ad6eddbac27c84ad605bebff7bf414afc
mathieu.pinaud@laplateforme.io <mathieu.pinaud@laplateforme.io>
2023-03-17 12:39:17 +0100

    plus d'inputs

--- main.py
+++ main.py
@@ -13,10 +13,7 @@ def my_check_tuple(my_tuple):
         return(False)
     return(True)
 
-#la fonction ne sert a rien mais il etait demandé dans le sujet qu'il y ai une autre fonction prenant en parametre un tuple pour regler l'alarme
-def my_reglage_alarme(tuple):
-    list = [tuple[0], tuple[1], tuple[2]]
-    return(list)
+
 
 #foncion pour dupliquer une liste afin de ne pas modifier la liste de base
 def my_dup_list(o_list):
@@ -55,6 +52,8 @@ def my_print_heure(time_list, t):
         print(' PM')
     elif (t == 2 and check == 2):
         print(' AM')
+    else:
+        print('')
 
 #menu demandant a l'utilisateur si il souhaite creer ou non une alarme
 def my_menu_alarm():
@@ -64,29 +63,34 @@ def my_menu_alarm():
         print('Saisie incorrecte')
         return(my_menu_alarm())
     if (i == 1):
-        return(my_set_alarm())
+        return(my_set_alarm(my_get_tuple(2)))
     elif (i == 2):
         return(None)
     else:
         print('Saisie incorrecte')
         return(my_menu_alarm())
 
+def my_get_tuple(i):
+    if i == 2:
+        print("Renseignez l'heure a laquelle l'alarme doit s'activer : ", end='')
+    elif i == 1:
+        print("Renseignez l'heure de démarage de l'horloge : ", end='')
+    try:
+        my_tuple = (int(input()), int(input("Précissez les minutes : ")), int(input("Précisez les secondes : ")))
+    except ValueError:
+        print('Saisie incorrecte')
+        return(my_get_tuple(i))
+    if (my_check_tuple(my_tuple) == True):
+        return(my_tuple)
+    else:
+        return(my_get_tuple(i))
+
+
 #fonction pour determiner l'heure de l'alarme avec securité du format
-def my_set_alarm():
+def my_set_alarm(time_tuple):
         
-        stop_time = []
-        try:
-            stop_time += [int(input("Renseignez l'heure a laquelle l'alarme doit s'activer : "))]
-            stop_time += [int(input("Précissez les minutes : "))]
-            stop_time += [int(input("Précisez les secondes : "))]
-        except ValueError:
-            print("Saisie incorrecte")
-            return(my_set_alarm())
-        if (my_check_tuple((stop_time[0], stop_time[1], stop_time[2])) == False):
-            print("Saisie incorrecte")
-            return(my_set_alarm())
-        else:
-            return(my_reglage_alarme((stop_time[0], stop_time[1], stop_time[2])))
+        stop_time = [time_tuple[0], time_tuple[1], time_tuple[2]]
+        return(stop_time)
 
 #verifie si l'heure actuelle correspond a celle de l'alarme
 def my_check_alarm(act_time, stop_time):
@@ -134,4 +138,5 @@ def afficher_heure(my_tuple):
 
 
 
-afficher_heure((12, 59, 45))
+time_tuple = my_get_tuple(1)
+afficher_heure(time_tuple)
